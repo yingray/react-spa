f741a3c79dde3120cba933c54ea59e34da8c8130
Yingray <yingray.lu@fuhu.com>
2017-01-23 11:18:58 +0800

    updated routes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { pages } from '../routes';
 
 class Nav extends React.Component {
     render() {
         return (
             <nav>
                 <ul>
-                    <li><Link className="active" to="/default"><i className="material-icons">style</i> React SPA</Link></li>
-                    <li><Link to="/"><i className="material-icons">home</i></Link></li>
-                    <li><Link to="/"><i className="material-icons">class</i></Link></li>
-                    <li><Link to="/"><i className="material-icons">info</i></Link></li>
-                    <li><Link to="/"><i className="material-icons">perm_contact_calendar</i></Link></li>
+                    <li><Link className="active" to="/"><i className="material-icons">style</i> React SPA</Link></li>
+                    {pages.map(page => <li><Link to={page.path}><i className="material-icons">home</i></Link></li>)}
+                    {/*<li><Link to="/"><i className="material-icons">class</i></Link></li>*/}
+                    {/*<li><Link to="/"><i className="material-icons">info</i></Link></li>*/}
+                    {/*<li><Link to="/"><i className="material-icons">perm_contact_calendar</i></Link></li>*/}
                 </ul>
             </nav>
         )
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/App';
+import Index from './pages/Default';
 
-import Default from './pages/Default';
+export const pages = [{
+    path: 'default', component: require('./pages/Default').default
+}];
 
 const routes = (
-	<Route path="/" component={App}>
-		<IndexRoute component={Default}/>
-		<Route path="default" component={Default}/>
+    <Route path="/" component={App}>
+        <IndexRoute component={Index}/>
+        {pages.map(page => <Route path={page.path} component={page.component}/>)}
     </Route>
 );
 
